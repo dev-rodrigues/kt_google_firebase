bae45e5b38ae83d1a8fae7f860b686c7cf844844
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2021-05-19 21:55:49 -0300

    tentando abrir activity

--- app/src/main/java/br/edu/infnet/connecttofirebase/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/connecttofirebase/MainActivity.kt
@@ -1,11 +1,100 @@
 package br.edu.infnet.connecttofirebase
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.View
+import androidx.core.content.FileProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+//    private fun dispatchTakePictureIntent() {
+//        // Intent Implícita
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { it ->
+//            it.resolveActivity(packageManager)?.also { it ->
+//                startActivityForResult(it, 1)
+//            }
+//        }
+//    }
+    val REQUEST_IMAGE_CAPTURE = 1
+//    private fun dispatchTakePictureIntent(view: View) {
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//            takePictureIntent.resolveActivity(packageManager)?.also {
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//            }
+//        }
+//    }
+
+    var imageBitmap : Bitmap? = null
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+        // Capturando a imagem da Intent
+            imageBitmap = data!!.extras!!.get("data") as Bitmap
+            imgVwLivroCapa.setImageBitmap(imageBitmap)
+        }
+    }
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+        // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    println(ex)
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "br.edu.infnet",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    fun teste(view: View) {
+        dispatchTakePictureIntent()
     }
 }
